0e33402dba99f1d442d0c65f913d37bd21cae445
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2026-03-26 16:30:28 +0100

    add event when product is added

--- mfe-header/src/components/Navbar.jsx
+++ mfe-header/src/components/Navbar.jsx
@@ -12,13 +12,6 @@ function Navbar() {
     return () => unsub();
   }, []);
 
-  useEffect(() => {
-    const unsub = eventBus.on('catalog:addtocart', () => {
-      console.log('Produit ajouté au panier !');
-    });
-    return () => unsub();
-  }, []);
-
   return (
     <nav className="navbar">
       <div className="navbar-brand">
